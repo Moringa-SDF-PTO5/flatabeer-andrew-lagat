86ffb81665cb7b50a2d665992d9cf27ae4a138e0
maruands <maruands@gmail.com>
2024-01-21 17:38:30 +0300

    display data done

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 var ourdata;
+var each_beer;
 const json_link = "http://localhost:3000/beers";
 fetch('db.json')
 .then(res => {
@@ -14,29 +15,51 @@ fetch('db.json')
     {
         let card = document.createElement("ul");
         card.className = "card";
-        //console.log(beers[i].name);
+        each_beer = beers[i];
         card.innerHTML = `
-            
             <div class="beer">
-                
-                <div class = "col-25">
-                    <h3>${beers[i].name}</h3>
-                </div>
-                <div class = "col-75">
-                   
-                </div>
-            </div>          
-        
+                <a onClick="beer('${beers[i].id}')"><h3>${beers[i].name}</h3></a>
+            </div>  
         `;
         document.querySelector('#beers').appendChild(card);
+        
+        //function that calls each beer
         
     }
         
     //console.log(data);
 })
 
-
-for(data in ourdata)
+function beer(x)
 {
-    console.log(data);
+    document.querySelector('#beer').innerHTML = '';
+    //console.log(x)
+    console.log(ourdata['beers'][x-1].reviews[0]);
+    var img = document.createElement('li');
+    
+
+    img.innerHTML = `
+        <h2>${ourdata['beers'][x-1].name}</h2>
+        <img src="${ourdata['beers'][x-1].image_url}" alt="" class="beer_img" srcset="">
+        <p>${ourdata['beers'][x-1].description}</p>
+        <h3>Reviews</h3>
+        <ul id='reviews'>
+
+        </ul>
+        
+       `
+    //console.log(img);
+    document.querySelector('#beer').appendChild(img);
+    
+
+    var review_list = document.createElement('li');
+    
+    for(let k = 0; k < ourdata['beers'][x-1].reviews.length; k++)
+    {
+        review_list.innerHTML = `
+            <li>${ourdata['beers'][x-1].reviews[k]}</li>
+        `
+    }
+
+    document.querySelector('#reviews').appendChild(review_list);
 }
